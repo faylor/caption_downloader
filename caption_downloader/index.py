077eacc97fde058336f148884980299d75867aad
faylor <32422243+faylor@users.noreply.github.com>
2020-11-30 09:12:19 +0000

    utf-8-sig added

--- caption_downloader/index.py
+++ caption_downloader/index.py
@@ -32,11 +32,12 @@ app.secret_key = 'sdfas2345tsrgtsdf'
 
 @app.route('/')
 def index():
-  return print_index_table()
+    return print_index_table()
+
 
 @app.route('/json/<video>')
 def json_captions(video):
-    info_url = "https://www.youtube.com/get_video_info?video_id=" + video #fU5X0cUMHac
+    info_url = "https://www.youtube.com/get_video_info?video_id=" + video  # fU5X0cUMHac
     response = requests.get(info_url)
     if response.status_code == 200:
         left_identifier = "captionTracks"
@@ -50,7 +51,7 @@ def json_captions(video):
         captions = jj["captions"]["playerCaptionsTracklistRenderer"]["captionTracks"]
         video_details = jj["videoDetails"]
         video_details["captions"] = captions
-        if len(captions) > 0 :
+        if len(captions) > 0:
             response_xml = requests.get(captions[0]["baseUrl"])
             video_details["CaptionData"] = xmltodict.parse(response_xml.text)
             return video_details
@@ -59,65 +60,66 @@ def json_captions(video):
 
 
 def get_video(videoId):
-    info_url = "https://www.youtube.com/get_video_info?video_id=" + videoId #fU5X0cUMHac
+    info_url = "https://www.youtube.com/get_video_info?video_id=" + videoId  # fU5X0cUMHac
     response = requests.get(info_url)
     if response.status_code == 200:
-      parsed = urllib.parse.parse_qs(response.text)
-      player_response = json.loads(parsed["player_response"][0])
-      video_details = player_response["videoDetails"]
-      if "captions" in player_response:
-        captions = player_response["captions"]["playerCaptionsTracklistRenderer"]["captionTracks"]
-        video_details["captions"] = captions
-        if len(captions) > 0 :
-          response_xml = requests.get(captions[0]["baseUrl"])
-          video_details["CaptionData"] = json.dumps(xmltodict.parse(response_xml.text))
-      else:
-        video_details["captions"] = {}
-        video_details["CaptionData"] = {}
-      return video_details
+        parsed = urllib.parse.parse_qs(response.text)
+        player_response = json.loads(parsed["player_response"][0])
+        video_details = player_response["videoDetails"]
+        if "captions" in player_response:
+            captions = player_response["captions"]["playerCaptionsTracklistRenderer"]["captionTracks"]
+            video_details["captions"] = captions
+            if len(captions) > 0:
+                response_xml = requests.get(captions[0]["baseUrl"])
+                video_details["CaptionData"] = json.dumps(xmltodict.parse(response_xml.text))
+        else:
+            video_details["captions"] = {}
+            video_details["CaptionData"] = {}
+        return video_details
     else:
-      return None
+        return None
 
 
 @app.route('/csv/<video>')
 def csv_captions(video):
     video_details = []
     if "," in video:
-      videos = video.split(",")
-      for vid in video.split(","):
-        if vid is not None and len(vid)>4:
-          dets = get_video(vid)
-          if dets is not None:
-            video_details.append(dets)
-      file_name =  "data.csv"
+        videos = video.split(",")
+        for vid in video.split(","):
+            if vid is not None and len(vid) > 4:
+                dets = get_video(vid)
+                if dets is not None:
+                    video_details.append(dets)
+        file_name = "data.csv"
     else:
-      tmp = get_video(video)
-      if tmp is None:
-        abort(404)
-      file_name = tmp["videoId"] + ".csv"
-      video_details.append(tmp)
+        tmp = get_video(video)
+        if tmp is None:
+            abort(404)
+        file_name = tmp["videoId"] + ".csv"
+        video_details.append(tmp)
 
     try:
-      dir_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "tmp")
-      file_path = os.path.join(dir_path, file_name)
-      fields = list(video_details[0].keys())
-      if "keywords" not in fields:
-        fields.append("keywords")
-      if "isLowLatencyLiveStream" not in fields:
-        fields.append("isLowLatencyLiveStream")
-      if "latencyClass" not in fields:
-        fields.append("latencyClass")
-      with open(os.path.join(dir_path, file_name), 'w') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=fields, quotechar='"', quoting=csv.QUOTE_NONNUMERIC,)
-        writer.writeheader()
-        for vid_det in video_details:
-          writer.writerow(vid_det)
-      return flask.send_from_directory(directory = dir_path, filename = file_name, as_attachment=True)
+        dir_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "tmp")
+        file_path = os.path.join(dir_path, file_name)
+        fields = list(video_details[0].keys())
+        if "keywords" not in fields:
+            fields.append("keywords")
+        if "isLowLatencyLiveStream" not in fields:
+            fields.append("isLowLatencyLiveStream")
+        if "latencyClass" not in fields:
+            fields.append("latencyClass")
+        with open(os.path.join(dir_path, file_name), 'w', encoding='utf-8-sig', newline='') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=fields, quotechar='"', quoting=csv.QUOTE_NONNUMERIC )
+            writer.writeheader()
+            for vid_det in video_details:
+                writer.writerow(vid_det)
+        return flask.send_from_directory(directory=dir_path, filename=file_name, as_attachment=True)
     except Exception as e:
         print("I/O error:" + str(e))
         abort(500)
 
-    return flask.jsonify(response.text)
+    return abort(400)
+
 
 # @app.route('/test')
 # def test_api_request():
@@ -223,30 +225,31 @@ def csv_captions(video):
 
 
 def credentials_to_dict(credentials):
-  return {'token': credentials.token,
-          'refresh_token': credentials.refresh_token,
-          'token_uri': credentials.token_uri,
-          'client_id': credentials.client_id,
-          'client_secret': credentials.client_secret,
-          'scopes': credentials.scopes}
+    return {'token': credentials.token,
+            'refresh_token': credentials.refresh_token,
+            'token_uri': credentials.token_uri,
+            'client_id': credentials.client_id,
+            'client_secret': credentials.client_secret,
+            'scopes': credentials.scopes}
+
 
 def print_index_table():
-  return ('<table>' +
-          '<tr><td><a href="/csv/dKHlY_grLDM,EQHb-ydSAV8,CFmM-vXH5F4,i1H8tuBLuWc,wMhex3CKC9c,0OksvP4nqek,p06YWEY36Jg,OcqC86vIwDs,TObXX28aE1M,lm-w7hVQpM4,hh9ktcnX5gg,oiDxmQEoS-g,sonx24bvMOo,etJkpBlElZI,ngBRRS6Sf0c,20_3BxUjXJo,_-9m0P0KuP8,Uxx5_QyXIKE,AeA5A6tb5Uk,nTJhNnfnsgI,LNHychYJ-6s,3f-q_yFsD1A,tTG4etv5Gi8,vY36Hxbafzo,lkKR4cz6nts,ODhRubWiPnQ,dW04jBwvZ9g,SB2KcJqejPM,SZD57UbsopA,S2KimmWo7Gc,pEOcAhWuTiE,za9rvKM3EE4,Wp7j7UfDJFM,Rn5a_ZPXKDM,VWFkW8G0YUI,H4I4YrfByp8,Yu_TQGMU60E,EZYtNOBw_58,9Au8a9FwZ48,PZyd4wWLp4w,ES1VD1E5I70,UrHhCsful4c,qTtzj8RF_V0,jjcvLH30RXI,N6nbQozepIQ,GSIUJVoXzSU,7IzaFW1jZ6o,LrykUOeuPfc,FYKheujTqE4,UFwyoYOPzLQ,ge1Nm5KNV4A,-FRJ38E7Ioo,E2aXOSJEc6M,IYQcULqe798,v1hmFu_61iM">Get a CSV of Captions</a></td>' +
-          '<td>Add Your Own To Test, Change Url to /csv/<video_id>.</td></tr>' +
-          '<tr><td><a href="/csv/dKHlY_grLDM,EQHb-ydSAV8,CFmM-vXH5F4,fU5X0cUMHac">Get a CSV of Captions</a></td>' +
-          '<td>Add Your Own To Test, Change Url to /csv/<video_id>.</td></tr>' +
-          '<tr><td><a href="/json/CFmM-vXH5F4">Get a JSON of Captions</a></td>' +
-          '<td>Add Your Own To Test, Change Url to /json/<video_id>.</td></tr>' +
-          '</table>')
+    return ('<table>' +
+            '<tr><td><a href="/csv/dKHlY_grLDM,EQHb-ydSAV8,CFmM-vXH5F4,i1H8tuBLuWc,wMhex3CKC9c,0OksvP4nqek,p06YWEY36Jg,OcqC86vIwDs,TObXX28aE1M,lm-w7hVQpM4,hh9ktcnX5gg,oiDxmQEoS-g,sonx24bvMOo,etJkpBlElZI,ngBRRS6Sf0c,20_3BxUjXJo,_-9m0P0KuP8,Uxx5_QyXIKE,AeA5A6tb5Uk,nTJhNnfnsgI,LNHychYJ-6s,3f-q_yFsD1A,tTG4etv5Gi8,vY36Hxbafzo,lkKR4cz6nts,ODhRubWiPnQ,dW04jBwvZ9g,SB2KcJqejPM,SZD57UbsopA,S2KimmWo7Gc,pEOcAhWuTiE,za9rvKM3EE4,Wp7j7UfDJFM,Rn5a_ZPXKDM,VWFkW8G0YUI,H4I4YrfByp8,Yu_TQGMU60E,EZYtNOBw_58,9Au8a9FwZ48,PZyd4wWLp4w,ES1VD1E5I70,UrHhCsful4c,qTtzj8RF_V0,jjcvLH30RXI,N6nbQozepIQ,GSIUJVoXzSU,7IzaFW1jZ6o,LrykUOeuPfc,FYKheujTqE4,UFwyoYOPzLQ,ge1Nm5KNV4A,-FRJ38E7Ioo,E2aXOSJEc6M,IYQcULqe798,v1hmFu_61iM">Get a CSV of Captions</a></td>' +
+            '<td>Add Your Own To Test, Change Url to /csv/<video_id>.</td></tr>' +
+            '<tr><td><a href="/csv/dKHlY_grLDM,EQHb-ydSAV8,CFmM-vXH5F4,fU5X0cUMHac">Get a CSV of Captions</a></td>' +
+            '<td>Add Your Own To Test, Change Url to /csv/<video_id>.</td></tr>' +
+            '<tr><td><a href="/json/CFmM-vXH5F4">Get a JSON of Captions</a></td>' +
+            '<td>Add Your Own To Test, Change Url to /json/<video_id>.</td></tr>' +
+            '</table>')
 
 
 if __name__ == '__main__':
-  # When running locally, disable OAuthlib's HTTPs verification.
-  # ACTION ITEM for developers:
-  #     When running in production *do not* leave this option enabled.
-  os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+    # When running locally, disable OAuthlib's HTTPs verification.
+    # ACTION ITEM for developers:
+    #     When running in production *do not* leave this option enabled.
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
-  # Specify a hostname and port that are set as a valid redirect URI
-  # for your API project in the Google API Console.
-  app.run('localhost', 8080, debug=True)
+    # Specify a hostname and port that are set as a valid redirect URI
+    # for your API project in the Google API Console.
+    app.run('localhost', 8080, debug=True)
